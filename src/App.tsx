42e4f7b3f38deaae5ec4fddabccf7b50247c90d0
BatanAndrei <yabatan777@gmail.com>
2023-10-25 17:31:56 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import Layout from './Layout/Layout';
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
 
 function App() {
 
   return (
-    <>
-     
-    </>
+      <>
+            <Layout>
+                <Header />
+                <Main />
+                <Footer />
+            </Layout>
+      </>
   )
 }
 
---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { TchildrenProps } from '../Types/Types';
+
+function Layout({ children }: TchildrenProps) {
+    return (
+        <>
+            { children}
+        </>
+    )
+}
+
+export default Layout;
