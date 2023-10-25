8c727abd1257d93ff35bd8a0346c4ee3c9110397
BatanAndrei <yabatan777@gmail.com>
2023-10-25 17:40:31 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import Layout from './Layout/Layout';
 import Header from './Header/Header';
 import Main from './Main/Main';
 import Footer from './Footer/Footer';
+import { Provider } from 'react-redux';
+
 
 function App() {
 
   return (
-      <>
+    <>
+        <Provider store={store}>
             <Layout>
                 <Header />
                 <Main />
                 <Footer />
             </Layout>
-      </>
+        </Provider>
+    </>
   )
 }
 
