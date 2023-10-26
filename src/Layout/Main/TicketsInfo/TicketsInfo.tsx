e1972e7a0c32f2cbf631981a3b906cec0cff0c9d
BatanAndrei <yabatan777@gmail.com>
2023-10-26 15:19:56 +0300

    initial commit

--- src/Layout/Main/TicketsInfo/TicketsInfo.tsx
+++ src/Layout/Main/TicketsInfo/TicketsInfo.tsx
@@ -2,9 +2,9 @@ import style from './ticketsInfo.module.css';
 
 function TicketsInfo() {
     return (
-        <>
-            
-        </>
+        <div className={style.ticket}>
+
+        </div>
     )
 }
 
