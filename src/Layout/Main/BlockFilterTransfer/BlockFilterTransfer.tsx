19915885c2595b7c6c006debcd1362d6d5327202
BatanAndrei <yabatan777@gmail.com>
2023-10-26 14:44:50 +0300

    initial commit

--- src/Layout/Main/BlockFilterTransfer/BlockFilterTransfer.tsx
+++ src/Layout/Main/BlockFilterTransfer/BlockFilterTransfer.tsx
@@ -8,22 +8,22 @@ function BlockFilterTransfer() {
                 <label className={style.label}>
                     <input type="checkbox" className={style.checkbox} />
                     <span className={style.fake}></span>
-                    <span className={style.first_checkbox_text}>Без пересадок</span>
+                    <span>Без пересадок</span>
                 </label>
                 <label className={style.label}>
                     <input type="checkbox" className={style.checkbox} />
                     <span className={style.fake}></span>
-                    <span className={style.first_checkbox_text}>1 пересадка</span>
+                    <span>1 пересадка</span>
                 </label>
                 <label className={style.label}>
                     <input type="checkbox" className={style.checkbox} />
                     <span className={style.fake}></span>
-                    <span className={style.first_checkbox_text}>2 пересадки</span>
+                    <span>2 пересадки</span>
                 </label>
                 <label className={style.label}>
                     <input type="checkbox" className={style.checkbox} />
                     <span className={style.fake}></span>
-                    <span className={style.first_checkbox_text}>3 пересадки</span>
+                    <span>3 пересадки</span>
                 </label>
             </div>
         </div>
