0d40663379471942e78ba58d549b7e3473eb8805
BatanAndrei <yabatan777@gmail.com>
2023-10-26 11:35:40 +0300

    initial commit

--- src/Layout/Main/BlockFilterCompanies/BlockFilterCompanies.tsx
+++ src/Layout/Main/BlockFilterCompanies/BlockFilterCompanies.tsx
@@ -3,6 +3,10 @@ import style from './blockFilterCompanies.module.css';
 function BlockFilterCompanies() {
     return (
         <div className={style.BlockFilterCompanies}>
+            <h2 className={style.titleFilter}>Компании</h2>
+            <div>
+                
+            </div>
         </div>
     )
 }
--- src/Layout/Main/BlockFilterTransfer/BlockFilterTransfer.tsx
+++ src/Layout/Main/BlockFilterTransfer/BlockFilterTransfer.tsx
@@ -2,7 +2,30 @@ import style from './blockFilterTransfer.module.css';
 
 function BlockFilterTransfer() {
     return (
-        <div className={style.BlockFilterTransfer}>
+        <div className={style.blockFilterTransfer}>
+            <h2 className={style.titleFilter}>Количество пересадок</h2>
+            <div className={style.positionCheckbox}>
+                <label className={style.label}>
+                    <input type="checkbox" className={style.checkbox} />
+                    <span className={style.fake}></span>
+                    <span className={style.first_checkbox_text}>Без пересадок</span>
+                </label>
+                <label className={style.label}>
+                    <input type="checkbox" className={style.checkbox} />
+                    <span className={style.fake}></span>
+                    <span className={style.first_checkbox_text}>1 пересадка</span>
+                </label>
+                <label className={style.label}>
+                    <input type="checkbox" className={style.checkbox} />
+                    <span className={style.fake}></span>
+                    <span className={style.first_checkbox_text}>2 пересадки</span>
+                </label>
+                <label className={style.label}>
+                    <input type="checkbox" className={style.checkbox} />
+                    <span className={style.fake}></span>
+                    <span className={style.first_checkbox_text}>3 пересадки</span>
+                </label>
+            </div>
         </div>
     )
 }
