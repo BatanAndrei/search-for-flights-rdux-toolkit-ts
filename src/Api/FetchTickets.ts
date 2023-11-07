39e53903482ea52692fed76c8381188c887c9150
BatanAndrei <yabatan777@gmail.com>
2023-11-07 12:06:44 +0300

    initial commit

--- src/Api/FetchTickets.ts
+++ src/Api/FetchTickets.ts
@@ -9,20 +9,20 @@ export const fetchTickets = createAsyncThunk<Iticket[], IparamsFetch, { rejectVa
     "tickets/fetchTickets", 
     async ({limit, companyP,companyR, companyS, transfers0, transfers1, transfers2, transfers3, sortPrice, sortDuratuon }: IparamsFetch, thunkApi) => { // объект thunkApi содержит функцию rejectWithValue
     
-      const response = await fetch(`http://localhost:3000/tickets?_limit=${limit}&company=${companyP}&company=${companyR}&company=${companyS}&transfers=${transfers0}&transfers=${transfers1}&transfers=${transfers2}&transfers=${transfers3}&_sort=${sortPrice},${sortDuratuon}&_order=asc`);
+        const response = await fetch(`http://localhost:3000/tickets?_limit=${limit}&company=${companyP}&company=${companyR}&company=${companyS}&transfers=${transfers0}&transfers=${transfers1}&transfers=${transfers2}&transfers=${transfers3}&_sort=${sortPrice},${sortDuratuon}&_order=asc`);
   
-      const data: Iticket[] = await response.json();
+        const data: Iticket[] = await response.json();
 
-      if (response.status !== 200) {
+        if (response.status !== 200) {
         
-          return thunkApi.rejectWithValue({  //rejectWithValue проверяет и возвращает ошибку при не удачном запросе
-          message: "Failed to fetch tickets." 
-          });
-      }
+            return thunkApi.rejectWithValue({  //rejectWithValue проверяет и возвращает ошибку при не удачном запросе
+            message: "Failed to fetch tickets." 
+            });
+      } 
 
         return data; // возврвщвет данные при успешном запросе
     }
-  );
+);
   
 
   
