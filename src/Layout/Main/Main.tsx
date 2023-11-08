b02edcd56ab7535bdfb07b2df4dfd22d8b63724f
BatanAndrei <yabatan777@gmail.com>
2023-11-08 14:59:53 +0300

    initial commit

--- src/Layout/Main/Main.tsx
+++ src/Layout/Main/Main.tsx
@@ -14,7 +14,7 @@ function Main() {
 
     const stateParams = useAppSelector(selectStatePrams);
     const stateDisplayFilter = useAppSelector(selectStateDisplayFilter);
-    
+
     const dispatch = useAppDispatch();
 
     useEffect(() => {
@@ -33,22 +33,17 @@ function Main() {
                 <div className={style.block_info__navigation}>
                     <ButtonsNavigation />
                 </div>
-
-
-                <div className={style.menuApearHeader}>
+                <div className={stateDisplayFilter ? style.menuApearHeaderWhithBottom : style.menuApearHeader}>
                     <h2 className={style.menuApear_title}>Любая авиакомпания, любое кол-во пересадок</h2>
                     <h2 className={style.menuApear_titleAction}>Открыть настройки</h2>
                     <SvgArrowDown />
-
+                    {stateDisplayFilter && 
                     <div className={style.menuApearFilter}>
                         <BlockFilterCompanies />
                         <BlockFilterTransfer />
                     </div>
-
+                    }
                 </div>
-                
-
-
                 <div className={style.block_info__tickets}>
                     {listTickets.map((ticket) => <TicketsInfo key={ticket.id} {...ticket} /* id={ticket.id} company={ticket.company} from={ticket.from} to={ticket.to} currency={ticket.currency} price={ticket.price} startTime={ticket.startTime} endTime={ticket.endTime} duration={ticket.duration} transfers={ticket.transfers} */ />)}
                 </div>
